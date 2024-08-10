d1f89428bd145182d42c6db2e3dcd244cd468c46
srivalli-ux <venkata.d.repudi@kipi.bi>
2024-08-10 13:04:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
      # st.subheader(fruit_chosen + ' Nutrition Information')
  import requests
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
- st.text(fruityvice_response)
+ st.text(fruityvice_response.json())
 
      
      
