77c7f173376d8bd5b875a1680b88d99e98cbfdb9
srivalli-ux <venkata.d.repudi@kipi.bi>
2024-08-10 13:51:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,22 +33,18 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-        ingredients_string = ''
-        for fruit_chosen in ingredients_list:
-                ingredients_string +=fruit_chosen + ' '
-                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-                st.subheader(fruit_chosen + ' Nutrition Information')
-# import requests
-                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-# st.text(fruityvice_response.json())
-                fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)    
-    
-
+ ingredients_string = ''
+ for fruit_chosen in ingredients_list:
+      ingredients_string +=fruit_chosen + ' '
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      st.subheader(fruit_chosen + ' Nutrition Information')
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+      fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
  #st.write(ingredients_string)
 
-                my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                          values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
+ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
  
  #st.write(my_insert_stmt)
  #st.stop()
